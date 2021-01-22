64d22e7617fc3670b41ffa52796875a207ad9214
abdulk1 <khan.abdul1@outlook.com>
2021-01-22 16:01:45 -0500

    Moving @types/leaflet to devDependencies

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,29 +1,28 @@
-import * as L from "leaflet";
-declare module "leaflet" {
-  class RotatedMarker extends Marker {
-    constructor(
-      latlng: LatLngExpression,
-      options: RotatedMarkerOptions | MarkerOptions
-    );
-    /*
-     * Sets the rotation angle value.
-     */
-    setRotationAngle(newAngle: number): void;
+import { LatLngExpression, Marker, MarkerOptions } from "leaflet";
 
-    /*
-     * Sets the rotation origin value.
-     */
-    setRotationOrigin(newOrigin: string): void;
-  }
+export class RotatedMarker extends Marker {
+  constructor(
+    latlng: LatLngExpression,
+    options: RotatedMarkerOptions | MarkerOptions
+  );
+  /*
+   * Sets the rotation angle value.
+   */
+  setRotationAngle(newAngle: number): void;
 
-  interface RotatedMarkerOptions extends MarkerOptions {
-    /*
-     * Rotation angle, in degrees, clockwise. Defaults to 0.
-     */
-    rotationAngle?: number;
-    /*
-     * Rotation angle, in degrees, clockwise. Defaults to 'bottom center'
-     */
-    rotationOrigin?: string;
-  }
+  /*
+   * Sets the rotation origin value.
+   */
+  setRotationOrigin(newOrigin: string): void;
+}
+
+interface RotatedMarkerOptions extends MarkerOptions {
+  /*
+   * Rotation angle, in degrees, clockwise. Defaults to 0.
+   */
+  rotationAngle?: number;
+  /*
+   * Rotation angle, in degrees, clockwise. Defaults to 'bottom center'
+   */
+  rotationOrigin?: string;
 }
